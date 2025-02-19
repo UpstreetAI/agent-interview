9ea6eee6f166bd2e8694e532d36f051c5ee203d9
Avaer Kazmer <ultimus@gmail.com>
2025-02-18 16:00:00 -0800

    Add generate-text.mjs

---
+++ src/lib/generate-text.mjs
@@ -0,0 +1,47 @@
+import { zodResponseFormat } from 'openai/helpers/zod';
+import { defaultModels } from '../constants.mjs';
+
+export const getTextGenerationConfig = () => {
+  return {
+    model: defaultModels[0],
+    key: process.env.OPENAI_API_KEY,
+  };
+};
+
+export const fetchJsonCompletion = async (opts, format) => {
+  const {
+    model = defaultModels[0],
+    key = null,
+    messages,
+    stream,
+  } = opts ?? {};
+
+  const response_format = format && zodResponseFormat(format, 'result');
+
+  const u = `https://api.openai.com/v1/chat/completions`;
+  const res = await fetch(u, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${key}`,
+    },
+    body: JSON.stringify({
+      model,
+      messages,
+      response_format,
+      stream,
+    }),
+  });
+  if (res.ok) {
+    const j = await res.json();
+    let content = j.choices[0].message.content;
+    if (format) {
+      content = JSON.parse(content);
+    }
+    return content;
+  } else {
+    const text = await res.text();
+    throw new Error('error response in fetch completion: ' + res.status + ': ' + text);
+  }
+};
+
