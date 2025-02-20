1bc68d6a46cb34aa053099df8c9a67c9a6d7fee7
Avaer Kazmer <ultimus@gmail.com>
2025-02-19 18:25:54 -0800

    Update test.ts

--- src/test.ts
+++ src/test.ts
@@ -1,24 +1,34 @@
 import {
-  // Readable,
-  // Writable,
   PassThrough,
 } from 'stream';
 import dotenv from 'dotenv';
 import {
   createAgent,
   // editAgent,
-  ReactAgentsRegistry,
-  ElizaosRegistry,
 } from './api.ts';
+import {
+  ReactAgentsRegistry,
+} from './registries/react-agents/react-agents-registry.ts';
+import {
+  ElizaosRegistry,
+} from './registries/elizaos/elizaos-registry.ts';
+
+//
 
 const test = async () => {
+  {
+    const registry = new ElizaosRegistry();
+    const plugins = await registry.getAllPlugins();
+    console.log('elizaos plugins', plugins);
+  }
+
   const events = new EventTarget();
   const inputStream = new PassThrough();
   inputStream.end();
   const outputStream = new PassThrough();
   outputStream.pipe(process.stdout);
 
-  const registry = new ElizaosRegistry();
+  const registry = new ReactAgentsRegistry();
   const agent = await createAgent({
     prompt: 'You are Donald Trump.',
     events,
